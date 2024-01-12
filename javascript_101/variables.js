8b9e3044fd5218cbd668c51b09316f68370f5567
jainmradul676 <83226470+jainmradul676@users.noreply.github.com>
2024-01-12 07:46:12 +0000

    Variables in javascript

---
+++ javascript_101/variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "mradulmj@gmail.com"
+var accountPassword = "qwerty"
+accountCity = "Jaipur" // this can also be used to reserve memory for variable
+
+// console.log(accountCity);
+
+// accountId=5 -- not allowed const update
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+/* 
+   We don't use var in javascript
+   Due to block scope and functional scope
+*/
