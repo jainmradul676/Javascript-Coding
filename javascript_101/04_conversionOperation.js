5afbd83ae329b530f9c4d65fcbb1c565954627fc
jainmradul676 <83226470+jainmradul676@users.noreply.github.com>
2024-01-13 02:26:18 +0000

    conversion operation lecture-1

---
+++ javascript_101/04_conversionOperation.js
@@ -0,0 +1,44 @@
+let score = 33
+// console.log(typeof score)
+
+let stringScore = String(score)
+
+// console.log(stringScore);
+// console.log(typeof stringScore);
+
+let value = null;
+
+// console.log(typeof value);
+let numberValue = Number(value);
+
+console.log(numberValue);
+console.log(typeof numberValue);
+
+let score1 = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN -- Not a number
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
